c4ddefa787aa6cb7158f7b426d614d7db2f1d1e1
Pierre Macedo <pierre.alexander.mg@gmail.com>
2018-09-05 15:31:38 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-/* This app shows the local weather of any city in the world using geolocation. All data is retrieved from an API using AJAX.
-
-Language: Javascript
-Library: jQuery
-Framework: Bootstrap
-
-Developed by: Pierre Macedo
-*/
-
-
 // Get coordinates
 function getLocation() {
 if (navigator.geolocation) {
